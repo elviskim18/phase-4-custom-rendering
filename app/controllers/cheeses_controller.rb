07e3180c9b79e2996356befbf5bb79ef4cf0c314
Kimani <elvis.kimani@student.moringaschool.com>
2022-09-27 13:09:16 +0300

    Done.

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -6,10 +6,18 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+ 
   # GET /cheeses/:id
   def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+   cheese = Cheese.find_by(id: params[:id])
+   if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+   else
+    render json: { error: 'Cheese not found' }, status: :not_found
+   end
   end
+
+
+ 
 
 end
